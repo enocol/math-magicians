4e18927b7a70f14ed27b426e43c914e246249944
Enoh <eno.col@gmail.com>
2023-09-20 00:07:14 +0100

    Edit Calculator.ja

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,17 +9,17 @@ const Calculator = () => (
       <div className="items item2 light-bg">+/-</div>
       <div className="items item3 light-bg">%</div>
       <div className="items item4 golden-bg">/</div>
-      <div className="items item5 light-bg">4</div>
-      <div className="items item6 light-bg">6</div>
-      <div className="items item7 light-bg">7</div>
+      <div className="items item5 light-bg">7</div>
+      <div className="items item6 light-bg">8</div>
+      <div className="items item7 light-bg">9</div>
       <div className="items item8 golden-bg">x</div>
-      <div className="items item9 light-bg">8</div>
-      <div className="items item10 light-bg">10</div>
-      <div className="items item11 light-bg">11</div>
+      <div className="items item9 light-bg">4</div>
+      <div className="items item10 light-bg">5</div>
+      <div className="items item11 light-bg">6</div>
       <div className="items item12 golden-bg">+</div>
-      <div className="items item13 light-bg">13</div>
-      <div className="items item14 light-bg">14</div>
-      <div className="items item15 light-bg">15</div>
+      <div className="items item13 light-bg">1</div>
+      <div className="items item14 light-bg">2</div>
+      <div className="items item15 light-bg">3</div>
       <div className="items item16 golden-bg">-</div>
       <div className="items item17 light-bg">0</div>
       <div className="items item18 golden-bg">=</div>
