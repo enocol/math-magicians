27f214837554c2acd241ddcbaa9b5a722b3c3571
Enoh <eno.col@gmail.com>
2023-09-19 19:34:58 +0100

    Edit Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,3 +1,29 @@
-const Calculator = () => <div> Calculator here</div>;
+import Results from './Results';
+
+const Calculator = () => (
+  <div className="container">
+    <section className="section">
+      <Results />
+      <div className="items item1">1</div>
+      <div className="items item2">2</div>
+      <div className="items item3">3</div>
+      <div className="items item4">/</div>
+      <div className="items item5">4</div>
+      <div className="items item6">6</div>
+      <div className="items item7">7</div>
+      <div className="items item8">x</div>
+      <div className="items item9">8</div>
+      <div className="items item10">10</div>
+      <div className="items item11">11</div>
+      <div className="items item12">+</div>
+      <div className="items item13">13</div>
+      <div className="items item14">14</div>
+      <div className="items item15">15</div>
+      <div className="items item16">-</div>
+      <div className="items item17">0</div>
+      <div className="items item18">=</div>
+    </section>
+  </div>
+);
 
 export default Calculator;
