6727712b7aa12151105fdb9765de26f6ea3edd10
Enoh <eno.col@gmail.com>
2023-09-19 23:54:44 +0100

    Edit Results.js

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Results = () => <div className="results">0</div>;
+const Results = (props) => {
+  const { value } = props;
+  return <div className="results gray-bg">{value}</div>;
+};
 
 export default Results;
+
+Results.propTypes = {
+  value: PropTypes.number.isRequired,
+};
