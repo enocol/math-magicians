8e4e835dfbcea96fc3cdd10e05c0d4239c1246b5
Enoh <eno.col@gmail.com>
2023-09-22 10:00:53 +0100

    Edit quotes.js

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
 const apiEndPoint = 'https://api.api-ninjas.com/v1/quotes?category=happiness';
 
-const Quotes = () => {
+const Quotes = ({ state }) => {
   const [quotes, setQuotes] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
 
   useEffect(() => {
+    setisLoading(true);
     const fetchdata = async () => {
       const response = await fetch(apiEndPoint, {
         method: 'GET',
@@ -14,18 +17,29 @@ const Quotes = () => {
       const json = await response.json();
 
       setQuotes(json);
+      setisLoading(false);
     };
 
     fetchdata();
-  }, [setQuotes]);
+  }, [setQuotes, setisLoading, state.total]);
+
+  if (isLoading) return <div className="loading">Loading......</div>;
 
   return (
     <div className="quote">
       {quotes.length === 0
-        ? 'loading......................................'
+        ? 'loading...............'
         : quotes.map((q) => <h1 key={q.quote}>{q.quote}</h1>)}
     </div>
   );
 };
 
 export default Quotes;
+
+Quotes.propTypes = {
+  state: PropTypes.string,
+};
+
+Quotes.defaultProps = {
+  state: {},
+};
